13b28b4cacd1c1eecfbd0171237755a538c07be4
jiuylq <jiuylqc@gmail.com>
2020-07-08 10:05:17 +0800

    add dropComponent

---
+++ src/views/Drop.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="dropbox">
+    <cardDragger :data="cardList"></cardDragger>
+  </div>
+</template>
+
+<script>
+import { cardDragger } from '@/compoments/carddragger'
+export default {
+  components: {
+    cardDragger
+  },
+  data () {
+    return {
+      cardList: []
+    }
+  },
+  beforeCreate () {
+  },
+  created () {
+    this.addDataList()
+  },
+  beforeMount () {},
+  mounted () {},
+  methods: {
+    addDataList () {
+      for (let i = 1; i < 30; i++) {
+        this.cardList.push({
+          positionNum: i,
+          name: '演示卡片' + i,
+          id: 'card' + i
+        })
+      }
+      console.log(this.cardList)
+    }
+  },
+  beforeDestroy () {},
+  destroyed () {}
+}
+</script>
+
+<style>
+
+</style>
